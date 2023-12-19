3a1c2261da536b047d0c70822c81805c5f800be8
Damjan Vincic <damjanvincic2003@gmail.com>
2023-12-19 15:37:37 +0100

    Add .gitignore, CMakeLists.txt, main.cpp and set up project structure

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// File: main.cpp
+// Author: Damjan Vincic
+// Date: December 19, 2023
+// Description: Main file for the program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
