1c8bc0d60c7f7cbcecb7cad7cc4e7daf778c7a57
Damjan Vincic <damjanvincic2003@gmail.com>
2023-12-21 00:24:50 +0100

    Add starting Sudoku9 class

---
+++ include/sudoku9.h
@@ -0,0 +1,73 @@
+/**
+ * @file sudoku9.h
+ * @brief Declaration of the Sudoku9 class
+ * @date December 21, 2023
+ * @author Damjan Vincic
+ */
+
+#ifndef SUDOKU_SUDOKU9_H
+#define SUDOKU_SUDOKU9_H
+#include <vector>
+
+/**
+ * @class Sudoku9
+ * @brief Class for sudoku representation and related methods
+ */
+class Sudoku9 {
+private:
+    /**
+     * @brief 2D vector representing the sudoku board
+     */
+    std::vector<std::vector<unsigned short>> board;
+
+    /**
+     * @brief Fill the diagonal subgrids of the sudoku board
+     */
+    void fillDiagonalSubgrids();
+public:
+    /**
+     * @brief Empty constructor for the Sudoku9 class
+     */
+    Sudoku9();
+
+    /**
+     * @brief Constructor for the Sudoku9 class
+     * @param board 2D vector representing the sudoku board
+     */
+    Sudoku9(const std::vector<std::vector<unsigned short>>& board);
+
+    /**
+     * @brief Copy constructor for the Sudoku9 class
+     * @param other Sudoku9 object to be copied
+     */
+    Sudoku9& operator=(const Sudoku9& other);
+
+    /**
+     * @brief Checks if a number can be placed in a given cell
+     * @param row Row of the cell
+     * @param col Column of the cell
+     * @param num Number to be placed
+     * @return True if the number can be placed, false otherwise
+     */
+    bool isSafe(int row, int col, unsigned short num);
+
+    /**
+     * @brief Solves the sudoku board
+     * @return True if the sudoku is solved, false otherwise
+     */
+    bool solveSudoku();
+
+    /**
+     * @brief Generates a sudoku board
+     */
+    void generateSudoku();
+
+    /**
+     * @brief Prints the sudoku board
+     */
+    void printBoard();
+
+    std::vector<unsigned short> operator[](int i);
+    const std::vector<unsigned short> operator[](int i) const;
+};
+#endif //SUDOKU_SUDOKU9_H
---
+++ src/sudoku9.cpp
@@ -0,0 +1,115 @@
+/**
+ * @file sudoku9.cpp
+ * @brief Implementation file for the Sudoku9 class
+ * @date December 21, 2023
+ * @author Damjan Vincic
+ */
+
+#include "../include/sudoku9.h"
+#include <iostream>
+
+Sudoku9::Sudoku9() : board(std::vector<std::vector<unsigned short>>(9, std::vector<unsigned short>(9, 0))) { }
+
+Sudoku9::Sudoku9(const std::vector<std::vector<unsigned short>>& board) : board(std::vector<std::vector<unsigned short>>(9, std::vector<unsigned short>(9, 0))) {
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j)
+            this->board[i][j] = board[i][j];
+    }
+}
+
+Sudoku9& Sudoku9::operator=(const Sudoku9& other) {
+    if (this != &other) {
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j)
+                board[i][j] = other[i][j];
+        }
+    }
+    return *this;
+}
+
+std::vector<unsigned short> Sudoku9::operator[](int i) {
+    return board[i];
+}
+
+const std::vector<unsigned short> Sudoku9::operator[](int i) const {
+    return board[i];
+}
+
+bool Sudoku9::isSafe(int row, int col, unsigned short num) {
+    // Check if the number is already in the row or column
+    for (int i = 0; i < 9; ++i) {
+        if (board[row][i] == num || board[i][col] == num)
+            return false;
+    }
+
+    // Check if the number is already in the 3x3 box
+    int boxRow = row - row % 3;
+    int boxCol = col - col % 3;
+    for (int i = boxRow; i < boxRow + 3; ++i) {
+        for (int j = boxCol; j < boxCol + 3; ++j) {
+            if (board[i][j] == num)
+                return false;
+        }
+    }
+
+    return true;
+}
+
+bool Sudoku9::solveSudoku() {
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            if (board[i][j] == 0) {
+                for (int num = 1; num <= 9; ++num) {
+                    if (isSafe(i, j, num)) {
+                        board[i][j] = num;
+                        if (solveSudoku())
+                            return true;
+                        board[i][j] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void Sudoku9::fillDiagonalSubgrids() {
+    for (int i = 0; i < 9; i += 3) {
+        for (int num = 1; num <= 9; ++num) {
+            int row = rand() % 3 + i;
+            int col = rand() % 3 + i;
+
+            while (!isSafe(row, col, num)) {
+                row = rand() % 3 + i;
+                col = rand() % 3 + i;
+            }
+            board[row][col] = num;
+        }
+    }
+}
+
+void Sudoku9::generateSudoku() {
+    srand(time(nullptr));
+    fillDiagonalSubgrids();
+    solveSudoku();
+    int numToRemove = rand() % 50 + 20;
+    for (int i = 0; i < numToRemove; ++i) {
+        int row = rand() % 9;
+        int col = rand() % 9;
+        if (board[row][col] != 0) {
+            board[row][col] = 0;
+        } else {
+            --i;
+        }
+    }
+}
+
+void Sudoku9::printBoard() {
+    for (std::vector<unsigned short>& row : board) {
+        for (unsigned short& num : row) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+    }
+}
