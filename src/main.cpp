62b5d7948946122ffd46322cbc897db149cc85dc
Damjan Vincic <damjanvincic2003@gmail.com>
2023-12-21 11:00:58 +0100

    Add game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,74 @@
-// File: main.cpp
-// Author: Damjan Vincic
-// Date: December 19, 2023
-// Description: Main file for the program
+/**
+ * @file main.cpp
+ * @brief Main file of the program
+ * @date December 21, 2023
+ * @author Damjan Vincic
+*/
 
 #include <iostream>
+#include "../include/sudoku9.h"
+#include "../include/sudoku_file_io.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    std::string challengeFilename = argv[1], solutionFilename = argv[2];
+    SudokuFileIO sudokuFileIO;
+    Sudoku9 sudoku;
+
+    char choice;
+    while (true) {
+        std::cout << "[1] Load a sudoku board.\n"
+                     "[2] Generate a sudoku board.\n"
+                     ">> ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case '1':
+                sudoku = sudokuFileIO.read(challengeFilename);
+                break;
+            case '2':
+                sudoku.generateSudoku();
+                sudokuFileIO.write(challengeFilename, sudoku);
+                break;
+            default:
+                std::cout << "Invalid input." << std::endl;
+                continue;
+        }
+
+        std::cout << "[1] Check solution validity\n"
+                     "[2] Solve the sudoku\n"
+                     ">> ";
+        std::cin >> choice;
+
+        switch(choice) {
+            case '1':
+                sudoku = sudokuFileIO.read(solutionFilename);
+                std::cout << (sudoku.checkSolutionValidity() ? "Solution valid." : "Solution invalid.") << std::endl;
+                break;
+            case '2':
+                sudoku.solveSudoku();
+                sudokuFileIO.write(solutionFilename, sudoku);
+                break;
+            default:
+                std::cout << "Invalid input." << std::endl;
+                continue;
+        }
+
+        std::cout << "[1] Continue\n"
+                     "[2] Exit\n"
+                     ">> ";
+
+        std::cin >> choice;
+
+        switch(choice) {
+            case '1':
+                break;
+            case '2':
+                return 0;
+            default:
+                std::cout << "Invalid input." << std::endl;
+                return 0;
+        }
+    }
+
     return 0;
 }
